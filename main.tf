fb89c99bc1406989085dcd3aa106e3a584f738a2
PadarbindaPuhan <Padarbinda.Puhan@gmail.com>
2025-05-20 20:26:14 +0530

    check sample

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
-# module "allow_all_security_group" {
-#   source = "./padarbindaorg/terraform_modules/securitygroup"
-#   environment = var.environment
-# }
+module "allow_all_security_group" {
+  source = "./platform-modules/terraform_modules/securitygroup"
+  environment = var.environment
+}
 
-# module "keypair" {
-#   source        = "./padarbindaorg/terraform_modules/keypair"
-#   key_name      = "dev-keypair"
-#   public_key    = var.keypair_public_key
-#   environment   = var.environment
+module "keypair" {
+  source        = "./platform-modules/terraform_modules/keypair"
+  key_name      = "dev-keypair"
+  public_key    = var.keypair_public_key
+  environment   = var.environment
   
-# }
+}
 
-# module "web_application_server" {
-#   source        = "./padarbindaorg/terraform_modules/ec2"
-#   ami           = "ami-075686beab831bb7f"
-#   instance_type = "t3.micro"
-#   subnet_id     = "subnet-0db2c89226baaf431"
-#   environment   = var.environment
-#   security_groups = [module.allow_all_security_group.security_group_id]
-#   key_pair      = module.keypair.key_name
-# }
+module "web_application_server" {
+  source        = "./platform-modules/terraform_modules/ec2"
+  ami           = "ami-075686beab831bb7f"
+  instance_type = "t3.micro"
+  subnet_id     = "subnet-0db2c89226baaf431"
+  environment   = var.environment
+  security_groups = [module.allow_all_security_group.security_group_id]
+  key_pair      = module.keypair.key_name
+}
